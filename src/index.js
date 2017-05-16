c1505b613218584638670ea5b9a6800f7aca578c
Marcus Rognes <marcus@rognes.guru>
2017-05-17 01:35:49 +0200

    Fixed duplicate export bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 export Form from './Form';
 export defaultFormInterface from './Form/defaultFormInterface';
 export FormItem from './FormItem';
-export FormItem from './FormItem';
 export SelectField from './Fields/SelectField';
 export TextareaField from './Fields/TextareaField';
 export TextField from './Fields/TextField';
