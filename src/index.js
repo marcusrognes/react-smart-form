412476a5510af4adb2e439d349d1476ca522f3f5
Marcus Rognes <marcus@rognes.guru>
2017-05-16 23:40:25 +0200

    Added ec6+ import entry

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export Form from './Form';
+export defaultFormInterface from './Form/defaultFormInterface';
+export FormItem from './FormItem';
+export FormItem from './FormItem';
+export SelectField from './Fields/SelectField';
+export TextareaField from './Fields/TextareaField';
+export TextField from './Fields/TextField';
